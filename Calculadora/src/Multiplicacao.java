7bfebb024b5cbc11edcbf9626c3f7bca727baa64
jefte.araujo <jefte.araujo@AN0700108077439.dinfor.uniceub>
2018-03-02 19:42:40 -0300

    1 - calculadora

---
+++ Calculadora/src/Multiplicacao.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Multiplicacao
+ */
+@WebServlet("/Multiplicacao")
+public class Multiplicacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Multiplicacao() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// recupera primeiro parametro
+		int numero1 = Integer.parseInt(request.getParameter("numero1"));
+		int numero2 = Integer.parseInt(request.getParameter("numero2"));
+
+		//Calculadora entrada = new Calculadora(numero1, numero2);
+		int resultado = numero1 * numero2;
+		
+		// pegar o writer do response
+		PrintWriter out = response.getWriter();
+
+		out.println("<html><heard><title>Calculadora</title></heard>");
+		out.println("<body>");
+		out.println(numero1 + " * " + numero2 + " = " + resultado);
+				
+		out.println("<body>");
+	}
+
+}
